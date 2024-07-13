32460becdfbc83b73a1cc8e695aca856eca36cc4
sunny kumar <sunnykr3913@gmail.com>
2024-07-14 02:22:14 +0530

    first-commit

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 ## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+Run `npm start` or `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
 ## Code scaffolding
 
@@ -12,16 +12,53 @@ Run `ng generate component component-name` to generate a new component. You can 
 
 ## Build
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+Run `npm run build` or `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+
+## Watching files
+
+Run `npm run watch` to build the project and watch for changes.
 
 ## Running unit tests
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+Run `npm test` or `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
 
 ## Running end-to-end tests
 
 Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
 
+## Using Tailwind CSS
+
+This project uses [Tailwind CSS](https://tailwindcss.com/). To customize the Tailwind configuration, modify the `tailwind.config.js` file.
+
+## Chart Integration
+
+This project uses [Chart.js](https://www.chartjs.org/) and [ng2-charts](https://valor-software.com/ng2-charts/) for charting. You can create various charts by using the `ng2-charts` components.
+
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## Dependencies
+
+- **Angular**: Core, Common, Compiler, Forms, Router, Animations, Platform-Browser, Platform-Browser-Dynamic
+- **Angular Material**: UI component library
+- **Angular CDK**: Component Dev Kit
+- **Chart.js**: Charting library
+- **ng2-charts**: Angular Chart.js integration
+- **@ng-web-apis/canvas**: Web APIs for Angular
+- **RxJS**: Reactive Extensions Library for JavaScript
+- **Zone.js**: Execution context library for JavaScript
+
+## Dev Dependencies
+
+- **Angular Devkit Build Angular**
+- **Angular CLI**
+- **Angular Compiler CLI**
+- **Typescript**
+- **Jasmine**: Testing framework
+- **Karma**: Test runner
+- **Karma Chrome Launcher**: Chrome launcher for Karma
+- **Karma Coverage**: Code coverage reporter for Karma
+- **Karma Jasmine**: Jasmine adapter for Karma
+- **Karma Jasmine HTML Reporter**: HTML reporter for Jasmine tests in Karma
+- **Tailwind CSS**: Utility-first CSS framework
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,284 @@
 import { Component } from '@angular/core';
+// import { Chart } from 'chart.js/auto'
+// import { NgChartjsService } from 'ng-chartjs';
+import { ChartData, ChartType } from 'chart.js';
+
+interface Workout {
+  name: string;
+  workouts: string;
+  numberOfWorkouts: number;
+  totalWorkoutMinutes: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'workout3';
+
+  workouts: Workout[] = [
+    {
+      name: 'John Doe',
+      workouts: 'Running, Cycling',
+      numberOfWorkouts: 2,
+      totalWorkoutMinutes: 75,
+    },
+    {
+      name: 'Jane Smith',
+      workouts: 'Swimming, Running',
+      numberOfWorkouts: 1,
+      totalWorkoutMinutes: 80,
+    },
+    {
+      name: 'Mike Johnson',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts: 4,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'Sunny Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts: 8,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'Praveen Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts: 7,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'Akash Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts: 32,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'Lakshay Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts: 2,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'Pradeep Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts: 12,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'Shaley Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts: 21,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'Kamlesh Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts: 32,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'Sarvesh Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'wsdcfvbvbhji Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'rfgbuyhgvjnh Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'rtyuikcvbnm Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'asdkjf Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'dfghjkl Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'wertyuio Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'sdfghjk Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'wertyuio Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'dfghjk Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'asdkasdjfa Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'jushs Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'xcvbnm Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'wertyuiollkjhgfd Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'sdfghjk Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'kailsh Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+    {
+      name: 'salens Kumar',
+      workouts: 'Yoga, Cycling',
+      numberOfWorkouts:342,
+      totalWorkoutMinutes: 90,
+    },
+  ];
+
+  newUserName: string = '';
+  newWorkoutType: string = '';
+  newWorkoutMinutes: number | null = null;
+
+  searchQuery: string = '';
+  filterType: string = 'All';
+
+  currentPage = 1;
+  itemsPerPage = 3;
+
+  selectedWorkout: Workout = this.workouts[0];
+
+  get paginatedWorkouts(): Workout[] {
+    const filteredWorkouts = this.filterWorkouts(this.workouts);
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    return filteredWorkouts.slice(start, end);
+  }
+
+  filterWorkouts(workouts: Workout[]): Workout[] {
+    return workouts.filter(
+      (workout) =>
+        (this.filterType === 'All' ||
+          workout.workouts.includes(this.filterType)) &&
+        workout.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
+
+  addWorkout(): void {
+    if (
+      this.newUserName &&
+      this.newWorkoutType &&
+      this.newWorkoutMinutes !== null
+    ) {
+      const existingWorkout = this.workouts.find(
+        (workout) => workout.name === this.newUserName
+      );
+
+      if (existingWorkout) {
+        existingWorkout.workouts += `, ${this.newWorkoutType}`;
+        existingWorkout.numberOfWorkouts += 1;
+        existingWorkout.totalWorkoutMinutes += this.newWorkoutMinutes;
+      } else {
+        this.workouts.push({
+          name: this.newUserName,
+          workouts: this.newWorkoutType,
+          numberOfWorkouts: 1,
+          totalWorkoutMinutes: this.newWorkoutMinutes,
+        });
+      }
+
+      this.newUserName = '';
+      this.newWorkoutType = '';
+      this.newWorkoutMinutes = null;
+    }
+  }
+
+  selectWorkout(workout: Workout): void {
+    this.selectedWorkout = workout;
+  }
+
+  nextPage(): void {
+    if (this.currentPage * this.itemsPerPage < this.workouts.length) {
+      this.currentPage++;
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  get chartData(): ChartData<'bar'> {
+    const workoutTypes = this.selectedWorkout.workouts.split(', ');
+    const workoutMinutes = workoutTypes.map(
+      () =>
+        this.selectedWorkout.totalWorkoutMinutes /
+        this.selectedWorkout.numberOfWorkouts
+    );
+
+    return {
+      labels: workoutTypes,
+      datasets: [
+        {
+          label: 'Minutes',
+          data: workoutMinutes,
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1,
+        },
+      ],
+    };
+  }
+
+  get chartLabels(): string[] {
+    return this.selectedWorkout.workouts.split(', ');
+  }
 }
